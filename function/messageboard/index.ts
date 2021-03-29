bf4e75b56099ff81528d72fa430f24ac3604f6fd
butterdoge <2967504101@qq.com>
2021-03-29 20:25:55 +0800

    完成了留言板的删除和随机查看功能。

--- function/messageboard/index.ts
+++ function/messageboard/index.ts
@@ -3,7 +3,22 @@ import logger from '../../lib/logger';
 import config from '../../config';
 import fs from 'fs';
 import path from 'path';
+//对「留言数目」初始化。
+const initTotal = () => {
+    let Boardmessages: Array<any> = getBoardMessage();//我为这行代码的any道个歉，忍不住偷懒。
+    let max: number=0;
+    Boardmessages.forEach((value) => {
+        // 求取最大的uid
+        if (value["mid"] > max)
+            max = value["mid"];
+    })
+    // const total = Boardmessages.length;
+    BoardMessage.currentID=max+1;
+}
+
+
 // 创建文件夹的部分完成,同时返回指定json对象。
+// 只读操作。
 const getBoardMessage = () => {
 	const wordPath = path.join(__dirname, "../../data/messageboard/board.json")
     if (!fs.existsSync(wordPath)) {
@@ -19,12 +34,12 @@ const getBoardMessage = () => {
     // 这边用object不知道为什么会有问题，待研究。
 }
 
-const update = (file: any) => {
+const Mupdate = (file: any) => {
 	try {
 		fs.writeFileSync((path.join(__dirname, "../../data/messageboard/board.json")), JSON.stringify(file));
-		logger("Word").info("词库文件写入成功");
+		logger("messageboard").info("留言板文件读取成功");
 	} catch (error) {
-		logger("Word").warn("词库文件写入失败", error);
+		logger("messageboard").warn("留言板文件读取失败", error);
 	}
 }
 
@@ -42,11 +57,12 @@ class BoardMessage{
         this.time = time;
         this.uid = uid;
         // 要是有构造函数重载我也不会用这有点别扭的写法。
+        // 如果该词条不存在的话选用新添ID。
         if (mid == -1) {
             this.mid = BoardMessage.currentID;
             BoardMessage.currentID++;
         }
-           
+        //    如果存在的话，具有显式id，就用原先的id。
         else {
             this.mid = mid;
         }
@@ -59,20 +75,22 @@ class BoardMessage{
         内容，如下：
         ${this.content}`;
         // 不这样改的话下面reply会报错，不能直接返回text，不知道为什么，待研究。
-        return `这条留言的时间是:${this.time.getFullYear()}年${this.time.getMonth()+1}月${this.time.getDate()}日。
-        写下这篇留言的人是:${this.user};
+        return `
+        📆这条留言的时间是:${this.time.getFullYear()}年${this.time.getMonth() + 1}月${this.time.getDate()}日。
+        📝写下这篇留言的人是:${this.user};
         留言的序列号为:${this.mid};
         内容，如下：
+
         ${this.content}`;
     }
 }
-
+initTotal();
 api.command(/^.写下留言(.*)$/, async (m, e, reply) => {
     let theMessage:BoardMessage = new BoardMessage(m[1], e.username, new Date(), e.uid)
     let Boardmessages:Array<BoardMessage> = getBoardMessage();
     Boardmessages.push(theMessage);
-    update(Boardmessages);
-    reply("这篇留言被记录下来了。",config.app.color);
+    Mupdate(Boardmessages);
+    reply("✒️这篇留言被记录下来了。",config.app.color);
 });
 
 api.command(/^.*读取留言.*$/, async (m, e, reply) => {
@@ -83,4 +101,47 @@ api.command(/^.*读取留言.*$/, async (m, e, reply) => {
         const text: string = itemMess.showText();
         reply(text, config.app.color);
     })
+})
+
+// 影响编号，删去一条条目。
+api.command(/^删除留言编号(\d+)*$/, async (m, e, reply) => {
+    // 其实现在只是个对象的数组,用Object比较合适，不知道为什么用boardmessage可以通过检查↑。
+    let Boardmessages: Array<Object> = getBoardMessage();
+    // 从对象数组的角度操作即可吧,不用全部转换成Message数组了，因为我不会转换。
+    let flag: boolean = false;
+    Boardmessages.forEach(function (item: any, index) {
+        // 这边应该不能用any，要用interface...看到这个代码的人对不起（x，会试着提高变成素养的。
+        //声明个interface有一丢丢小麻烦...
+        if (item["mid"] == m[1]) {
+            flag = true;
+            console.log(`匹配成功，该留言的id为${item["mid"]}`);
+            reply(
+            `❗️ 即将删去的消息内容为: ${item["content"]}
+             ❗️ 留下该留言的人为: ${item["user"]}
+            `, config.app.color)
+            if (e.username == item["user"]||e.username==config.master) {
+                Boardmessages.splice(index, 1);
+                reply("你撕下了这个留言。", config.app.color);
+                Mupdate(Boardmessages);
+            }
+            else {
+                reply("只可以是作者或者机器人主人撕下。", config.app.color);
+            }
+            
+        }
+            
+    })
+    if(flag==false)
+        reply("😥找不到你说的留言唷", config.app.color);
+})
+
+// 只读操作，不会影响编号。
+api.command(/^看看留言$/, async (m, e, reply) => {
+    let Boardmessages: Array<Object> = getBoardMessage();
+    const total: number = Boardmessages.length;
+    var num = Math.floor(Math.random() * total);
+    let targetPost: any = Boardmessages[num];
+    let itemMess = new BoardMessage(targetPost.content, targetPost.user, new Date(targetPost.time), targetPost.uid,targetPost.mid);
+    const text: string = itemMess.showText();
+    reply(text, config.app.color);
 })
