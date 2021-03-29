b8c1e05d7770e6475138fc486214fb40c4e240d5
butterdoge <2967504101@qq.com>
2021-03-29 18:49:50 +0800

    实现了公告板的id号，修补了签文的随机报错问题。

--- function/messageboard/index.ts
+++ function/messageboard/index.ts
@@ -30,26 +30,40 @@ const update = (file: any) => {
 
 // 创建信息域的部分。
 class BoardMessage{
+    static currentID: number=1;
     content: String;
     user: String;
     time: Date;
     uid: String;
-    constructor(content: String, user: String, time: Date,uid:String) {
+    mid: number;
+    constructor(content: String, user: String, time: Date,uid:String,mid:number=-1) {
         this.content = content;
         this.user = user;
         this.time = time;
         this.uid = uid;
+        // 要是有构造函数重载我也不会用这有点别扭的写法。
+        if (mid == -1) {
+            this.mid = BoardMessage.currentID;
+            BoardMessage.currentID++;
+        }
+           
+        else {
+            this.mid = mid;
+        }
+            
     }
+
     showText() {
-        let text: String = `这条留言的时间是:${this.time.getFullYear()}年，${this.time.getDate()}月，${this.time.getDay()}日。
-        写下这篇留言的人是:this.user;
+        let text: String = `这条留言的时间是:${this.time.getFullYear()}年，${this.time.getMonth()}月，${this.time.getDate()}日。
+        写下这篇留言的人是:${this.user};
         内容，如下：
-        this.content`;
+        ${this.content}`;
         // 不这样改的话下面reply会报错，不能直接返回text，不知道为什么，待研究。
-        return `这条留言的时间是:${this.time.getFullYear()}年，${this.time.getDate()}月，${this.time.getDay()}日。
-        写下这篇留言的人是:this.user;
+        return `这条留言的时间是:${this.time.getFullYear()}年${this.time.getMonth()+1}月${this.time.getDate()}日。
+        写下这篇留言的人是:${this.user};
+        留言的序列号为:${this.mid};
         内容，如下：
-        this.content`;
+        ${this.content}`;
     }
 }
 
@@ -64,6 +78,9 @@ api.command(/^.写下留言(.*)$/, async (m, e, reply) => {
 api.command(/^.*读取留言.*$/, async (m, e, reply) => {
     let Boardmessages: Array<BoardMessage> = getBoardMessage();
     Boardmessages.forEach(function (item) {
-        reply(item.showText(), config.app.color);
+        // json的parse只能默认转换成一个普通对象，而不是一个类，必须重新转换成一个类的对象才可以调用其方法。
+        let itemMess = new BoardMessage(item.content, item.user, new Date(item.time), item.uid,item.mid);
+        const text: string = itemMess.showText();
+        reply(text, config.app.color);
     })
 })
